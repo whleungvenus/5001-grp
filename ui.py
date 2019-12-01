2cf7156cbd142772a077dee80b4ef4f12556d27c
whleungvenus <57953109+whleungvenus@users.noreply.github.com>
2019-12-02 00:12:48 +0800

    ui
    ui

--- ui.py
+++ ui.py
@@ -1,65 +1,97 @@
 #import tkinter
 import tkinter as tk
 from tkinter import ttk
+import pandas as pd
+import csv
+import math  
 
+userList = pd.read_csv('candidates.csv', usecols=['user_id', 'taste_group'])
+print(userList)
+tasteGroup = []
+with open('taste_grp.csv', 'r') as f:
+  reader = csv.reader(f)
+  tastegrp = list(reader)
 
+with open('taste_groups.csv', 'r') as f:
+    reader = csv.reader(f)
+    tasteGroup = list(reader)
+    tasteGroup.remove(tasteGroup[0])
+#print(tasteGroup)
 
 
 master = tk.Tk()
 tabControl = ttk.Notebook(master)
 
 tab1 = ttk.Frame(tabControl)            # Create a tab 
-tabControl.add(tab1, text='Predict By Restaurant')      # Add the tab
-tab2 = ttk.Frame(tabControl)            # Add a second tab
-tabControl.add(tab2, text='Suggested by restautant type')
+tabControl.add(tab1, text='Recommendation')      # Add the tab
+#tab2 = ttk.Frame(tabControl)            # Add a second tab
+#tabControl.add(tab2, text='Suggested by restautant type')
 tabControl.pack(expand=1, fill="both") 
 
-#Predict By Restaurant
-tk.Label(tab1, text="Restaurant ID:").grid(row=0)
-tk.Label(tab1, text="User you should find:").grid(row=1)
+var2 = tk.StringVar(value=tastegrp)
+
+#创建Listbox
+
+lb = tk.Listbox(tab1, listvariable=var2,selectmode='multiple') 
+lb.grid(row=1,column=1,padx=(20,5),pady=20)
+
+finalScore  = []
+
 def clickPredictSubmit():
-    lbl = tk.Label(tab1, text="123")
-    lbl.grid(column=1, row=1)
-e1 = tk.Entry(tab1)
+    selectList = [idx for idx in lb.curselection()]        
+    finalScore  = []   
+    for grp in tasteGroup:
+        local_sum_square =0
+        for idx, val in enumerate(grp):
+            if(idx in selectList):
+                print(idx)
+                local_sum_square = local_sum_square +((float(val)-1)*(float(val)-1))
+            else:
+                local_sum_square = local_sum_square +(float(val))*(float(val))
+        #print(math.sqrt(local_sum_square))
+        finalScore.append(math.sqrt(local_sum_square))
+    #print('finalScore',min(finalScore))
+    #print('finalScore',finalScore.index(min(finalScore)))
+    showTargetUser(finalScore.index(min(finalScore)))   
+
+def showTargetUser(group):
+    user =userList[userList['taste_group'] == group]['user_id'].item()
+    tk.Label(tab1, text="User you should find:").grid(row=2)
+   
+    tk.Label(tab1, text=user).grid(row=2,column=1)
+
+                
+
 tk.Button(tab1, 
           text='Submit', 
           command=clickPredictSubmit).grid(row=3, 
                                     column=0, 
                                     sticky=tk.W, 
                                     pady=4)
-e1.grid(row=0, column=1)
+#e1 = tk.Entry(tab1)
+#e1.grid(row=0, column=1)
 #e2.grid(row=1, column=1)
 
 #Suggestion
 
-tk.Label(tab2, text="Restaurant Type:").grid(row=0)
-tk.Label(tab2, text="User you should find:").grid(row=1)
+#tk.Label(tab2, text="Restaurant Type:").grid(row=0)
+#tk.Label(tab2, text="User you should find:").grid(row=1)
 
 
-def clickTypeSubmit():
-    lbl = tk.Label(tab1, text="123")
-    lbl.grid(column=1, row=1)
-e1 = tk.Entry(tab1)
-tk.Button(tab1, 
-          text='Submit', 
-          command=clickTypeSubmit).grid(row=3, 
-                                    column=0, 
-                                    sticky=tk.W, 
-                                    pady=4)
+#def clickTypeSubmit():
+#    lbl = tk.Label(tab1, text="123")
+#    lbl.grid(column=1, row=1)
+#e1 = tk.Entry(tab1)
+#tk.Button(tab1, 
+#          text='Submit', 
+#          command=clickTypeSubmit).grid(row=3, 
+#                                    column=0, 
+#                                    sticky=tk.W, 
+#                                    pady=4)
 
 
 
 
-# li = ['王记','12','男']
-# tree = ttk.Treeview(tab2,columns=['1','2','3'],show='headings')
-# tree.column('1',width=100,anchor='center')
-# tree.column('2',width=100,anchor='center')
-# tree.column('3',width=100,anchor='center')
-# tree.heading('1',text='姓名')
-# tree.heading('2',text='学号')
-# tree.heading('3',text='性别')
-# tree.insert('','end',values=li)
-# tree.grid()
 
 
 master.mainloop()
